0d40e68b3897e48e67a9d109cbe2d56288eff0d4
Joel Hukubun <joelhukubun@gmail.com>
2020-03-01 19:34:47 +0800

    update kesekian kalinya
    update jumlah asset
    maklum noob

--- sw.js
+++ sw.js
@@ -6,14 +6,27 @@ const staticAssets = [
     '/index.html',
     '/css/style.css',
     '/js/index.js',
+    '/img/dev-joel-01.jpg',
+    '/img/dev-joel-02.jpg',
+    '/img/devjoel.png',
     '/img/icon_192.png',
+    '/img/icon_512.png',
+    '/img/portfolio-01.jpg',
+    '/img/portfolio-02.jpg',
+    '/img/portfolio-03.jpg',
+    '/img/portfolio-04.jpg',
+    '/img/portfolio-05.jpg',
+    '/img/portfolio-06.jpg',
+    '/img/portfolio-07.jpg',
+    '/img/portfolio-08.jpg',
+    '/img/portfolio-09.jpg',
+    '/img/portfolio-10.jpg',
+    '/img/services-bg.jpg',
     '/manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.css',
     'https://fonts.googleapis.com/css?family=Lora:400,700|Roboto+Slab:400,700&display=swap'
 ];
-
-
 
 self.addEventListener('install', async e => {
     const cache = await caches.open(cacheName);
